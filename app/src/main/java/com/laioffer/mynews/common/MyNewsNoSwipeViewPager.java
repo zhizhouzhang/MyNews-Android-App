dc42ebe3473a9568f11ba95ce268eab20676982c
zz134 <zz134@duke.edu>
2019-07-20 21:35:17 -0700

    add ViewPager

---
+++ app/src/main/java/com/laioffer/mynews/common/MyNewsNoSwipeViewPager.java
@@ -0,0 +1,27 @@
+package com.laioffer.mynews.common;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+public class MyNewsNoSwipeViewPager extends ViewPager {
+
+    public MyNewsNoSwipeViewPager(Context context) {
+        super(context);
+    }
+
+    public MyNewsNoSwipeViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return super.onInterceptTouchEvent(event);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
+}
